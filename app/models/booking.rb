8c05f3fb15aaea401e1fb14d7ed252691d2eb2be
martino vivien <vivien.martino@hotmail.fr>
2024-11-19 11:18:11 +0100

    validation des models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :beast
   belongs_to :user
+  validates :user_id, presence: true
+  validates :beast_id, presence: true
+  validates :date_start, presence: true
+  validates :date_end, presence: true
 end
