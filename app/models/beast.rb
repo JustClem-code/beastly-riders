3c9fb4579e2fcc11dd64159d67bbf03522f639be
martino vivien <vivien.martino@hotmail.fr>
2024-11-19 14:23:34 +0100

    seed operationelle

--- app/models/beast.rb
+++ app/models/beast.rb
@@ -1,12 +1,13 @@
 class Beast < ApplicationRecord
   has_many :users, through: :bookings, dependent: :destroy
   has_many_attached :photos
+  belongs_to :owner, class_name: "User"
 
   CATEGORY = ["💧 Aquatique 💧", "🪽 Volante 🪽", "🦄 Terrestre 🦄", "🕳️ Sous-terraine 🕳️"]
 
   validates :name, presence: true
   validates :description, presence: true
   validates :price_per_day, presence: true
-  validates :owner_id, presence: true
+  # validates :owner_id, presence: true
   validates :category, presence: true
 end
