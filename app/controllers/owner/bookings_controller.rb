6f819d30b243dfc0baf34fb9f292127b4791bf29
Diane Atticus <diane.atticus@laposte.net>
2024-11-20 16:20:22 +0100

    ajout du status created à la creation d'une reservation

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,13 +1,17 @@
 class Owner::BookingsController < ApplicationController
 
   def accept
+    # Récupére la reservation
     @booking = Booking.find(params[:id])
+    # Modifie la valeur associée à status en "validated"
     @booking.update_attibute(:status, "validated")
     redirect_to owner_bookings_path, status: :see_other
   end
 
   def decline
+    # Récupére la reservation
     @booking = Booking.find(params[:id])
+    # Modifie la valeur associée à status en "canceled"
     @booking.update_attibute(:status, "canceled")
     redirect_to owner_bookings_path, status: :see_other
   end
