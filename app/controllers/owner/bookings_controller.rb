6d32294070c4808dc0f839fe0f6022ba4d272399
Diane Atticus <diane.atticus@laposte.net>
2024-11-20 16:16:35 +0100

    ajout method accept et decline dans controller

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,2 +1,15 @@
 class Owner::BookingsController < ApplicationController
+
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.update_attibute(:status, "validated")
+    redirect_to owner_bookings_path, status: :see_other
+  end
+
+  def decline
+    @booking = Booking.find(params[:id])
+    @booking.update_attibute(:status, "canceled")
+    redirect_to owner_bookings_path, status: :see_other
+  end
+
 end
