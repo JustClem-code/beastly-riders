16362a4ca43ade1c6a6a19daa64dcd746f0208fd
Clement lemarie <big_m@live.fr>
2024-11-21 14:25:58 +0100

    bug fix delete

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :beast
   belongs_to :user
-  validates :user_id, presence: true
-  validates :beast_id, presence: true
   validates :date_start, presence: true
   validates :date_end, presence: true
   # Le statut peut être : "created", "validated", "payed", "canceled"
